b7e64662a3f399b9742c178054b740b834a7e09b
bettynjeri001 <bettywachira14@gmail.com>
2025-04-11 03:15:27 +0300

    icons change

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -1,18 +1,30 @@
-import React from 'react'
-import check from '../assets/check.png'
-import oval from '../assets/oval.png'
-import trash from '../assets/trash.png'
+import React from 'react';
+import { FaRegCircleCheck, FaRegCircle, FaTrash } from "react-icons/fa6";
 
-const TodoItems = ({text , id , isComplete , deleteTodo , toggle}) => {
+const TodoItems = ({ text, id, isComplete, deleteTodo, toggle }) => {
   return (
-    <div className='flex items-center my-3 gap-2'>
-      <div onClick={() => {toggle(id)}}className=' flex flex-1 items-center cursor-pointer'>
-        <img src={ isComplete ? check : oval} alt="" className='w-7 '/>
-        <p className={`text-black ml-4 text-[17px] ${isComplete ? "line-through" : "" }`}>{text}</p>
+    <>
+      <div className='flex items-center my-3 gap-2'>
+        <div
+          onClick={() => toggle(id)}
+          className='flex flex-1 items-center cursor-pointer'
+        >
+          {isComplete ? (
+            <FaRegCircleCheck className='text-green-500 w-6 h-6' />
+          ) : (
+            <FaRegCircle className='text-gray-400 w-6 h-6' />
+          )}
+          <p className={`ml-4 text-sm ${isComplete ? "line-through text-gray-500" : "text-black"}`}>
+            {text}
+          </p>
+        </div>
+        <FaTrash
+          onClick={() => deleteTodo(id)}
+          className='text-red-500 w-4 h-4 cursor-pointer'
+        />
       </div>
-      <img onClick={()=>{deleteTodo(id)}}src={trash} alt="" className='w-3.5  cursor-pointer'/>
-    </div>
-  )
-}
+    </>
+  );
+};
 
-export default TodoItems
+export default TodoItems;
